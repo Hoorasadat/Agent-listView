db6a0f83c42daeb60581d35001238428e650e5e3
Hoorasadat Mirsanei <805946@ACDM.DS.SAIT.CA>
2019-06-01 11:21:42 -0600

    test

--- app/src/main/java/com/example/Day12Examples/MainActivity.java
+++ app/src/main/java/com/example/Day12Examples/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
